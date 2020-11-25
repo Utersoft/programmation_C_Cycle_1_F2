15e3ee47e42f183c0beafcd7c939b5118ade9de1
Stephane Wilhelm <s.wilhelm@ludus-academie.com>
2020-11-25 10:58:34 +0100

    SLD2
    SDL2, delay, SDLquit, SDLinit

--- programmation_C/main.c
+++ programmation_C/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
 
-int main()
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 780
+
+
+SDL_Window *pWindow = NULL;
+SDL_Renderer *pRenderer = NULL;
+
+
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }else{
+        pWindow = SDL_CreateWindow("An SDL2 window", 0, 0,WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if(pWindow){
+            pRenderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_PRESENTVSYNC);
+
+        }
+    }
+
+
+    SDL_Delay(10000);
+
+    SDL_Quit();
+
+
     return 0;
 }
