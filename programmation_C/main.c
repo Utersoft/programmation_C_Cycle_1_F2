bb1830fcfd119aad5d4ae6143fb54f0d5ab3fc38
Stephane Wilhelm <s.wilhelm@ludus-academie.com>
2020-11-26 10:41:17 +0100

    .

--- programmation_C/main.c
+++ programmation_C/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //Exemple un point dans un plan
 
@@ -7,8 +8,20 @@ typedef struct tPoint{
 
     int x;
     int y;
+    char str_coul[10];
 
 }tPoint;
+
+//typedef enum couleur{red = 1, black, green}; il faut mettre énum devant la var
+typedef enum couleur{red = 1, black, green}couleur;
+
+typedef enum booleen{faux, vrai}booleen;
+
+
+//proto
+void setPoint(tPoint *pPoint, int n_x, int n_y, char str_coul[10]);
+
+
 
 /*struct tPoint{
 
@@ -17,15 +30,17 @@ typedef struct tPoint{
 
 }tPoint;*/
 //Fonctionne aussi mais il faut mettre un struct devant le type lorsque l'on crée une variable
-
+//Struct tPoint monPoint;
 
 int main()
 {
-    tPoint monPoint = {0, 0};
+    tPoint monPoint = {0, 0,{"rouge"}};
     //exemple :
     //struct tPoint monPoint;
 
     tPoint *pMonPoint = NULL;
+
+    tPoint tab_point[100];
 
     pMonPoint = &monPoint;
 
@@ -40,6 +55,33 @@ int main()
     (*pMonPoint).x = 20;
     (*pMonPoint).y = 12;
 
+    /*int i = 0;
+    for(i = 0; i < 100; i++){
+        tab_point[i].x = 0;
+        tab_point[i].y = 0;
+    }*/
+
+    monPoint.x = 0;
+    monPoint.y = 0;
+    //monPoint.str_coul = "black"; ne marche pas
+    strcpy(monPoint.str_coul, "black");
+
+    setPoint(&monPoint, 8, 8, "green");
+
+
+    //les enum
+    enum couleur cl1;
+    cl1 = 1;
+
+    booleen bln_val = faux;
 
     return 0;
+}
+
+
+void setPoint(tPoint *pPoint, int n_x, int n_y, char str_coul[10]){
+    pPoint->x = n_x;
+    pPoint->y = n_y;
+    strcpy(pPoint->str_coul, str_coul);
+
 }
